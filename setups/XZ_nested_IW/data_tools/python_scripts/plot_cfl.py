304c8709ee39bde76ea35cd46c7704d6ab0b49f4
kraig <kbwinters@ucsd.edu>
2021-05-03 15:31:05 -0700

    Update plot_cfl.py
    now takes root dir on command line, uses parse_problem)params to get dt

--- setups/XZ_nested_IW/data_tools/python_scripts/plot_cfl.py
+++ setups/XZ_nested_IW/data_tools/python_scripts/plot_cfl.py
@@ -1,6 +1,6 @@
 #!
 """
-    simple plot of cfl data
+    simple plot of cfl data, takes root_dir and tscale as a command line arguments
 """
 
 #---------------------------------------------------------------------------------------
@@ -9,24 +9,38 @@
 import os,math,sys              # Import the os and math modules
 import numpy as np
 import matplotlib
-#  THIS BACKEND WILL AVOID ANY CONNECTIONS TO A REMOTE X SERVER
-# Must be after importing matplotlib and before importing matplotlib.pyplot or pylab!
-matplotlib.use('Agg')
-#
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MultipleLocator, FormatStrFormatter
+from data_processing_utilities import parse_problem_params
 #-----------------------------------------------------------------------------------------
 
-#------------------------------------------------------------------------------------------
-#  may need to check paths depending on where "input"  is located relative to "flow_solve"
-#------------------------------------------------------------------------------------------
-data_file = '../../output/cfl.dat'     # t, cfl_x, cfl_y, cfl_z
-plot_dir = '../../output/figures/'
+root_dir = sys.argv[1]
+tscale = sys.argv[2]
+
+if(tscale=='s'):
+	xnorm = 1.
+elif(tscale=='hrs'):
+	xnorm = 3600.
+elif(tscale=='days'):
+	xnorm = (24.*3600.)
+	
+#--------------------------------------------------------------------------- 
+# root directory 
+# (below which the output directory containing the saved data files exists)
+#---------------------------------------------------------------------------
+#root_dir = '/Users/kraig/flow_solve_BC/'
+problem_params = parse_problem_params(root_dir)
+[runlabel,restart_flag,do_second_scalar,AB_order,p1,p2,nx,ny,nz,dt,t0,tf,Lx,Ly,Lz, \
+x_periodic,y_periodic,z_periodic,s1_def,s2_def,user_forcing_flag,rho0,g,f0,nu,     \
+kappa1,kappa2,high_order_flag,p,T_diff] = problem_params
+
+data_file = root_dir + 'output/cfl.dat'     # t, cfl_x, cfl_y, cfl_z
+plot_dir  = root_dir + 'output/figures/'
 plot_file = plot_dir + 'cfl.eps'
 
 if not os.path.exists(plot_dir):
-    cmd = 'mkdir -p ' + plot_dir
-    os.system(cmd)
+	cmd = 'mkdir -p ' + plot_dir
+	os.system(cmd)
 
 
 #---------------------------------------
@@ -35,21 +49,20 @@ if not os.path.exists(plot_dir):
 f = open(data_file, 'r')
 f = np.loadtxt(data_file)
 time = f[:,0] ; cfl_x = f[:,1]  ; cfl_y = f[:,2] ; cfl_z = f[:,3]
-dt = 60.0
 
 
 
 
-fig = plt.figure(figsize=(8,3.75),dpi=300)              # fig size in inches
-#plt.rc('text', usetex=True)
+fig = plt.figure(figsize=(8,3.75),dpi=300)                 # fig size in inches
+plt.rc('text', usetex=True)
 plt.rc('font', family='serif')
-axes = fig.add_axes([0.10, 0.15, 0.75, 0.75])    # lower, bottom, width, height in ( 0 to 1)
+axes = fig.add_axes([0.10, 0.15, 0.75, 0.75])              # lower, bottom, width, height in ( 0 to 1)
 axes.tick_params(direction='out', top=False, right=False,) # Turn ticks out
 axes.tick_params(axis='both', which='major', labelsize=12)
-axes.spines['top'].set_visible(False)                     # Get rid of top axis line
+axes.spines['top'].set_visible(False)                      # Get rid of top axis line
 axes.spines['right'].set_visible(False)
 
-time = time/(24.*3600)   # work in days
+time = time/xnorm   # work in "tscale"
 
 plt.plot(time,cfl_x,'r',label='cfl x')
 plt.plot(time,cfl_y,'g',label='cfl y')
@@ -60,9 +73,9 @@ plt.legend(loc='lower left')
 axis_lims = [time[0], 1.05*time[-1], 0., 0.25]
 plt.axis(axis_lims)
 
-axes.set_xlabel(r'$t \, \rm[days]$',fontsize=14)
-axes.set_ylabel(r'$(v,w)\,dt/(dy,dz)$',fontsize=14)
-title_string = r"low resolution parent_run:          dt=%.2f  [s] " %(dt)
+axes.set_xlabel(tscale,fontsize=14)
+axes.set_ylabel(r'$(u,v,w)\,dt/(dx,dy,dz)$',fontsize=14)
+title_string = "maximum cfl values vs time:          dt=%.4f  [s] " %(dt)
 axes.set_title(title_string,fontsize=12)
 
 
