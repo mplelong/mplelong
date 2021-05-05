27bce6085cbfb2c1782a359bd664828bb8d39fc7
kraig <kbwinters@ucsd.edu>
2021-05-04 20:49:20 -0700

    Update run.sh
    use $HOSTNAME, rm input link before creating a new one to TaylorGreen2D

--- setups/TaylorGreen2D/run.sh
+++ setups/TaylorGreen2D/run.sh
@@ -3,21 +3,23 @@
 # Script to compile flow_solve, create a clean output area, run the codes
 # and do some basic data management tasks
 #-------------------------------------------------------------------------
-echo run.sh now running on lacosta
+echo run.sh now running on "$HOSTNAME"
 
 # absolute pathname, able to deal w/ symlinks etc
 flow_solve_root="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-#------------------
-# prepare things
-#------------------
+#-------------------------------------------------
+# prepare things for Taylor-Green vortex test run
+#-------------------------------------------------
+rm -f input
+ln -s setups/TaylorGreen2D input
 make clean
 make flow.x
 make outdirs
 
-#------------------
-# do the run
-#------------------
+#---------------------------------------------------
+# do the run (it's set up for a 2x2 processor grid)
+#---------------------------------------------------
 mpirun -np 4 ./flow.x
 
 #---------------------------------------------------------
