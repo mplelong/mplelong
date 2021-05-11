c1d5c2336a2da1cdf63bf558ed6d3d287af34545
kraig <kraig@coast.ucsd.edu>
2021-05-11 10:24:53 -0700

    update Tri-periodic run.sh

--- setups/Tri-periodicExactSoln/run.sh
+++ setups/Tri-periodicExactSoln/run.sh
@@ -7,19 +7,23 @@ echo run.sh now running on "$HOSTNAME"
 
 source set_env_variables.sh
 
-#-------------------------------------------------
-# prepare things for Tri-periodicExactSoln test
-#-------------------------------------------------
-#rm -f input
-#ln -s setups/Tri-periodicExactSoln input
-#make clean
-#make flow.x
-#make outdirs
+do_run=True
 
-#---------------------------------------------------
-# do the run (it's set up for a 2x2 processor grid)
-#---------------------------------------------------
-#"$MPIRUN" -np 4 ./flow.x
+#------------------------------------------------------------
+# prepare things for Tri-periodic exact soln comparison test
+#------------------------------------------------------------
+if( "$do_run" ) then
+	rm -f input
+	ln -s setups/Tri-periodicExactSoln input
+	make clean
+	make flow.x
+	make outdirs
+
+	#---------------------------------------------------
+	# do the run (it's set up for a 4x1 processor grid)
+	#---------------------------------------------------
+	"$MPIRUN" -np 4 ./flow.x
+fi
 
 #---------------------------------------------------------------
 # when run is complete, stitch together the output files
@@ -27,10 +31,14 @@ source set_env_variables.sh
 # 2nd arg for plot_cfl.py is s, hrs or days for plot time scale
 #---------------------------------------------------------------
 cd input/data_tools
-#"$PYTHON" concatenate_results.py "$FLOW_SOLVE_ROOT"
+"$PYTHON" concatenate_results.py "$FLOW_SOLVE_ROOT"
 "$PYTHON" python_scripts/plot_cfl.py "$FLOW_SOLVE_ROOT"  s    # plot the time scale seconds
-"$PYTHON" python_scripts/compare_results.py "$FLOW_SOLVE_ROOT"  # plot comparison of computed vs exact sols
+"$PYTHON" python_scripts/compare_results.py "$FLOW_SOLVE_ROOT"  
 
+cd "$FLOW_SOLVE_ROOT"
+open -a Preview output/figures/cfl.pdf
 
 cd "$FLOW_SOLVE_ROOT"
 open -a Preview output/figures/cfl.pdf output/figures/comparison.pdf
+
+"$NCVIEW" output/slices/2D/XY* &
